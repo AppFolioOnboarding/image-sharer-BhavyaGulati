cc3cd5850bb86ac6f80fd4220ea5509e39a59e98
Bhavya Gulati <bhavya.gulati@appfolio.com>
2019-01-14 13:10:06 -0800

    bg - Adding Config files with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#index' # default path
-  # resources :application
+  root 'images#new' # default path
+  resources :images, only: %i[new create show]
 end
