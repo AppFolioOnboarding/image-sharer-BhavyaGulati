69f15c2f1cc701de2f684db2982072476525d0a1
Bhavya Gulati <bhavya.gulati@appfolio.com>
2019-01-29 12:04:43 -0800

    refactoring style to be rendered from react

--- app/javascript/image_sharing/components/Footer.js
+++ app/javascript/image_sharing/components/Footer.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+const divStyle = {
+  fontSize: 10
+};
+
 class Footer extends Component {
   /* Implement your Footer component here */
   render() {
     return (
-      <div>
+      <div style={divStyle}>
         <footer className='text-center' id='footer-text'>
           Copyright: AppFolio Inc. Onboarding
         </footer>
