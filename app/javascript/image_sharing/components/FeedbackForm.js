cdeb0f6134bab3fc459b10a4f29fa8fb069fbe01
Bhavya Gulati <bhavya.gulati@appfolio.com>
2019-01-31 11:42:29 -0800

    adding feedback form

---
+++ app/javascript/image_sharing/components/FeedbackForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Button, FormRow } from 'react-gears';
+import {observer, inject} from 'mobx-react'
+import PropTypes from 'prop-types';
+
+@inject('stores')
+@observer
+class FeedbackForm extends Component {
+
+  constructor (props){
+    super(props)
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleFeedbackChange = this.handleFeedbackChange.bind(this);
+  }
+
+  static propTypes = {
+    stores: PropTypes.objectOf(PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      feedback: PropTypes.string.isRequired,
+    }))
+  };
+
+  handleNameChange(event) {
+    this.props.stores.feedbackStore.setName(event.target.value);
+  }
+
+  handleFeedbackChange(event) {
+    this.props.stores.feedbackStore.setFeedback(event.target.value);
+  }
+
+
+  render() {
+    return (
+      <div className="formGroup">
+        <FormRow label="Your name" value={this.props.stores.feedbackStore.name} onChange={this.handleNameChange} labelSize="lg" width={{}} stacked/>
+        <FormRow type="textarea" label="Comment" value={this.props.stores.feedbackStore.feedback} onChange={this.handleFeedbackChange} labelSize="lg" width={{}} stacked/>
+        <Button color="primary" disabled={false} outline={false} >
+          Submit
+        </Button>
+      </div>
+    )
+  }
+}
+
+export default FeedbackForm;
